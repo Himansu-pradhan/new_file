d524e672158f867a99583564cedf017013f9c621
Himansu_20 <himansu2020@gift.edu.in>
2023-02-05 14:15:27 +0530

    first commit

---
+++ pw.java
@@ -0,0 +1,6 @@
+class pw{
+    public static void main(String[] args) {
+        
+    System.out.println("hello world");
+    }
+}
---
+++ pw2.java
@@ -0,0 +1,9 @@
+public class pw2 {
+    public static void main(String[] args) {
+        int a=45;
+        double b;
+        b=a;
+        System.out.println(b);
+    }
+    
+}
